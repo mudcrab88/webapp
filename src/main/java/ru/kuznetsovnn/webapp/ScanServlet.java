de6a8504e82a6be625574fc599dfae7662f3ef2b
Кузнецов Николай Николаевич <KuznecovNN@Depfin.local>
2020-07-08 16:45:12 +0400

    fix

--- src/main/java/ru/kuznetsovnn/webapp/ScanServlet.java
+++ src/main/java/ru/kuznetsovnn/webapp/ScanServlet.java
@@ -10,6 +10,7 @@ public class ScanServlet extends HttpServlet {
  
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        System.out.println("Start scanning...");
         request.getRequestDispatcher("/views/scan.jsp").forward(request, response);
     }
 }
